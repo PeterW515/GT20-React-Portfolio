a0b96ce223703b61cdba721bfc33ec2d810f3dba
Peter Weigman <phweigman@gmail.com>
2021-09-18 18:42:57 -0400

    cleaned up errors in banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,9 +7,9 @@ function Banner() {
         <div class="section-100vh">
             <div class="overlay-content">
                 <div class="parallax-container">
-                    <div class="parallax" style="background-image: url(assets/code_sample.png)">
+                    <div class="parallax" style={{backgroundImage:"url('assets/code_sample.png')"}}>
                     </div>
-                    <div class="overlay-text" style="width: 75%; min-width: 350px;">
+                    <div class="overlay-text" style={{width:"75%", minWidth:"350px"}}>
                         <h1 class="white-text">Peter Weigman's Portfolio</h1>
                         <a class="waves-effect waves-light btn" href="#myWork" id="enter">Enter</a>
                     </div>
