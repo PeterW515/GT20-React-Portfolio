80737d714d00a92378f20b0ecceb4661c0e0cf9d
Peter Weigman <phweigman@gmail.com>
2021-10-07 20:42:36 -0400

    completed footer

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import pages from './components/Main/pages';
 
 
 function App() {
   const [currentPage, setCurrentPage] = useState('About');
 
+  const pages = ['About', 'Portfolio', 'Contact', 'Resume']
+
   return (
     <div>
-      <Header pages={Object.keys(pages)} currentPage={currentPage} handlePageChange={setCurrentPage} />
+      <Header pages={pages} currentPage={currentPage} handlePageChange={setCurrentPage} />
       <Main currentPage={currentPage} />
       <Footer />
     </div>
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,32 @@
 import React from "react";
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
 
 
 
-function Main() {
+function Main({ currentPage }) {
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+    }
 
     return (
         <main>
             <h1>Main</h1>
+            {renderPage()}
         </main>
     );
 
