e02465b9f24aa43b9967e6cd31b724eb40af8ffa
Peter Weigman <phweigman@gmail.com>
2021-10-07 23:23:37 -0400

    updated resume

--- src/components/Main/Resume.js
+++ src/components/Main/Resume.js
@@ -1,10 +1,30 @@
 import React from "react";
 
 function Resume() {
+
+
     return (
-        <div>
-            <h1>Resume</h1>
+        <div className="row">
+            <div className="col m6 s12 center-align">
+
+                <ul className="collection with-header">
+                    <li className="collection-header"><i className="fas fa-server">Back End Skills</i></li>
+                    <li className="collection-item"><div>After graduating from Clemson University in 2018, I began working for Manhattan Associates, a supply chain software company. As a consultant and then business analyst, I worked on the functional side of the software. Around the middle of 2020, I began to think I wanted to move in to more technical work.</div></li>
+                </ul>
+
+            </div>
+            <div className="col m6 s12 center-align">
+
+                <ul className="collection with-header">
+                    <li className="collection-header"><i className="fas fa-desktop">Front End Skills</i></li>
+                    <li className="collection-item"><div>I've developed this portfolio with links to some of my projects, as well as my Github account. I hope to use this to showcase my work and launch my career as a full stack developer!</div></li>
+                </ul>
+
+            </div>
         </div>
+
+
+
     )
 }
 
